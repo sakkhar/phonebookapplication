ab489ff7ed4deb661e692391bc653aa69f007463
sahasakkhar <sakkhar@mail.com>
2017-11-17 01:50:50 +0600

    Django Simple Phonebook Application

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    first_name  =models.CharField(max_length=200, null=True, blank=True)
+    last_name   =models.CharField(max_length=200, null=True, blank=True)
+    phone_number= models.IntegerField(default=0, unique=True)
+    email       =models.EmailField()
+    location    =models.CharField(max_length=200, null=True, blank=True)
+    emergency_contact = models.IntegerField(default=0, unique=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
